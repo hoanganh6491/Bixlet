3fc279b3611ba98e40284c12c3c13ba8951d5d0f
Brian Gilham <me@briangilham.com>
2016-01-10 20:39:57 -0500

    Initial Commit

---
+++ Bixlet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bixlet
+//
+//  Created by Brian Gilham on 2016-01-10.
+//  Copyright © 2016 Brian Gilham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
