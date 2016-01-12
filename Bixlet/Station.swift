c42fa76e4bce7654c9f168067b2a9eaa9021ea6c
Brian Gilham <me@briangilham.com>
2016-01-11 21:11:04 -0500

    Created Station model.

--- Bixlet/Station.swift
+++ Bixlet/Station.swift
@@ -10,4 +10,46 @@ import Foundation
 import Argo
 import Curry
 
+struct Station {
+    let availableBikes: Int
+    let availableDocks: Int
+    let id: Int
+    let landMark: Int
+    let lastCommunicationTime: NSDate
+    let latitude: Double
+    let longitude: Double
+    let stationName: String
+    let statusKey: Int
+    let statusValue: String
+    let testStation: Bool
+    let totalDocks: Int
+}
 
+extension Station: Decodable {
+    static func decode(j: JSON) -> Decoded<Station> {
+        let f = curry(Station.init)
+            <^> j <| "availableBikes"
+            <*> j <| "availableDocks"
+            <*> j <| "id"
+            <*> j <| "landMark"
+        return f
+            <*> (j <| "lastCommunicationTime" >>- toNSDate)
+            <*> j <| "latitude"
+            <*> j <| "longitude"
+            <*> j <| "stationName"
+            <*> j <| "statusKey"
+            <*> j <| "statusValue"
+            <*> j <| "testStation"
+            <*> j <| "totalDocks"
+    }
+}
+
+let jsonDateFormatter: NSDateFormatter = {
+    let dateFormatter = NSDateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd' 'HH:mm:ss a" // 2016-01-11 20:40:57 PM
+    return dateFormatter
+}()
+
+let toNSDate: String -> Decoded<NSDate> = {
+    .fromOptional(jsonDateFormatter.dateFromString($0))
+}
